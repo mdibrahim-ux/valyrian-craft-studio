5dd9acedc7dc38337bc9430a23e302acf04f8056
mdibrahim-ux <mohammedibrahim07579@gmail.com>
2026-05-02 12:49:49 +0530

    fix alias path issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
-  base: "/",   // important
+  base: "/",
+
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),   // ✅ REQUIRED FIX
+    },
+  },
 });
